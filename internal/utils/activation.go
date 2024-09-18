7acfba6b866bc8fce300bbb929e90abdf3ae38ae
Juan Gabito <gabitoju@gmail.com>
2024-09-18 10:52:16 -0300

    Add softmax

--- internal/utils/activation.go
+++ internal/utils/activation.go
@@ -1,6 +1,10 @@
 package utils
 
-func relu(x float64) float64 {
+import (
+	"math"
+)
+
+func Relu(x float64) float64 {
 	if x < 0 {
 		return 0
 	}
@@ -12,4 +16,50 @@ func relu_derivative(x float64) float64 {
 		return 0
 	}
 	return 1
+}
+
+func Softmax(input [][]float64, dim int) [][]float64 {
+
+	output := make([][]float64, len(input))
+
+	if dim == 0 {
+
+		rows := len(input)
+		columns := len(input[0])
+
+		for i := range output {
+			output[i] = make([]float64, columns)
+		}
+
+		for i := 0; i < columns; i++ {
+			column := make([]float64, rows)
+			for j := 0; j < rows; j++ {
+				column[j] = input[j][i]
+			}
+
+			softmaxColumns := softmax1d(column)
+
+			for j := 0; j < rows; j++ {
+				output[j][i] = softmaxColumns[j]
+			}
+		}
+	} else {
+		for i, row := range input {
+			output[i] = softmax1d(row)
+		}
+	}
+
+	return output
+}
+
+func softmax1d(logits []float64) []float64 {
+	output := make([]float64, len(logits))
+	sum := 0.0
+	for i := range logits {
+		sum += math.Exp(logits[i])
+	}
+	for i := range logits {
+		output[i] = math.Exp(logits[i]) / sum
+	}
+	return output
 }
--- internal/utils/activation_test.go
+++ internal/utils/activation_test.go
@@ -19,7 +19,7 @@ func TestRELU(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			actual := relu(test.input)
+			actual := Relu(test.input)
 			if actual != test.expected {
 				t.Errorf("relu(%f) = %f; want %f", test.input, actual, test.expected)
 			}
@@ -47,6 +47,27 @@ func TestRELUDerivative(t *testing.T) {
 			actual := relu_derivative(test.input)
 			if actual != test.expected {
 				t.Errorf("relu_derivative(%f) = %f; want %f", test.input, actual, test.expected)
+			}
+		})
+	}
+}
+
+func TestSoftmax(t *testing.T) {
+	tests := []struct {
+		name   string
+		dim    int
+		input  [][]float64
+		output [][]float64
+	}{
+		{name: "simple_softmax_row", dim: 1, input: [][]float64{{1, 2, 3}, {4, 5, 6}}, output: [][]float64{{0.09003057317038046, 0.24472847105479764, 0.6652409557748219}, {0.09003057317038046, 0.24472847105479764, 0.6652409557748219}}},
+		{name: "simple_softmax_column", dim: 0, input: [][]float64{{1, 2, 3}, {4, 5, 6}}, output: [][]float64{{0.04742587317756678, 0.04742587317756678, 0.04742587317756678}, {0.9525741268224331, 0.9525741268224331, 0.9525741268224331}}},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := Softmax(test.input, test.dim)
+			if !EqualMatrices(actual, test.output, 1e-9) {
+				t.Errorf("softmax(%v, 1) = %v; want %v", test.input, actual, test.output)
 			}
 		})
 	}
